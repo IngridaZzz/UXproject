e87cb2c0ebbca93806074a84b5b7d1ea5a07cc74
Ingrida <ingridozmail@gmail.com>
2019-04-07 20:41:52 +0200

    api done
    api css

--- js/script.js
+++ js/script.js
@@ -2,35 +2,21 @@ $(document).ready(function () {
 
     addVideosToHtml();
 
-    collectAPIData();
+    //collectAPIData();
 
 });
-
-function collectAPIData() {
-    // document.getElementById("searchCityButton").addEventListener("click", function() {
-    var type = "annualavg";
-    var vari = "tas";
-    var start = "1920";
-    var end = "1939";
-    var country = "BRA";
-    var url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/" +
-        type + "/" + vari + "/" + start + "/" + end + "/" + country;
-    console.log(url);
-    collectData(url, showData);
-    //})
-}
 
 function addVideosToHtml() {
     var videoOne = document.getElementById("videoOne");
     var videoTwo = document.getElementById("videoTwo");
-    var videoThree = document.getElementById("videoThree");
+    //var videoThree = document.getElementById("videoThree");
 
-    videoOne.src = 'https://h5.veer.tv/player?vid=160766&utm_medium=embed';
-    videoOne.autoplay = true;
-    videoOne.muted = true;
+    //videoOne.src = 'https://h5.veer.tv/player?vid=160766&utm_medium=embed';
+    //videoOne.autoplay = true;
+    //videoOne.muted = true;
 
-    videoTwo.src = 'https://assets.veervr.tv/@veervr/blink/v0.8.6/embed/index.html?ixId=MtbjxDMRdIoavhue9xLHNJtmZCY&lang=en&utm_medium=embed';
-    videoThree.src = 'https://h5.veer.tv/player?vid=178636&utm_medium=embed';
+    //videoTwo.src = 'https://assets.veervr.tv/@veervr/blink/v0.8.6/embed/index.html?ixId=MtbjxDMRdIoavhue9xLHNJtmZCY&lang=en&utm_medium=embed';
+    //videoThree.src = 'https://h5.veer.tv/player?vid=178636&utm_medium=embed';
 }
 
 function collectData(url, callback_Function) {
@@ -39,7 +25,7 @@ function collectData(url, callback_Function) {
         if (this.readyState === 4 && this.status === 200) {
             callback_Function(this);
         }
-    };
+    }
     xhttp.open("GET", url, true);
     xhttp.send();
 }
@@ -55,6 +41,16 @@ $(document).ready(function () {
     $(document).keydown(function (e) {
         if (e.keyCode == '32') {
             $('#question').show();
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.container').show();
+
+    $(document).keydown(function (e) {
+        if (e.keyCode == '32') {
+            $('.container').hide();
         }
     });
 });
---
+++ js/weather.js
@@ -0,0 +1,30 @@
+// This is a HttpRequest to the openweather.org API where we get the content
+function collectData(url, callback_Function){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200){
+            callback_Function(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    
+} /* END of collectData - function */
+
+function showData(jsonData){
+     var jsonElements= JSON.parse(jsonData.responseText);
+    document.getElementById("showWeather").innerHTML =
+        "<h2>Location: "+jsonElements.name+", "+jsonElements.sys.country+"</h2>"+    
+        "<p>Temperature: "+jsonElements.main.temp+" &#8451;</p>"+
+        "<p>Pressure: "+jsonElements.main.pressure+"</p>"+
+        "<p>Humidity: "+jsonElements.main.humidity+"</p>"+
+        "<p>Min temp: "+jsonElements.main.temp_min+"</p>"+
+        "<p>Max temp: "+jsonElements.main.temp_max+"</p>"+
+        "<p>Wind: "+jsonElements.wind.speed+"</p>";
+}
+
+
+/* Main program */
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=brasilia&units=metric&appid=5f2e27f6971e8f85f633b0cd12187ca2";
+    collectData(url, showData);
+    
